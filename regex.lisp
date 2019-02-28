c82cc349492b4640fce49e491ffd8bbb71f9f346
TritiumMonoid <tritiummonoid@gmail.com>
2019-02-27 23:34:04 -0800

    add regex example

---
+++ regex.lisp
@@ -0,0 +1,24 @@
+(require "cl-ppcre")
+
+(defun continue? (string)
+  (cl-ppcre:scan "(?i)(y|ye|yes)" string))
+
+(defun is-variable? (string)
+  (cl-ppcre:scan "^[A-z][A-z0-9]*$" string))
+
+(defun is-number? (string)
+  (cl-ppcre:scan "^([\+\-])?[0-9]+(.[0-9]+)?((e|E)([\+\-])?[0-9]+)?$" string))
+
+(defun classify-lexeme (string)
+  (cond ((is-variable? string) 'VARIABLE)
+	((is-number? string) 'NUMBER)
+	(t 'NONE)))
+
+(defun prompt ()
+  (loop
+    (format *query-io* "LEXEME: ")
+    (force-output)
+    (format *query-io* "~a~%" (classify-lexeme (read-line *query-io*)))
+    (format *query-io* "CONTINUE [y/N]: ")
+    (force-output)
+    (when (null (continue? (read-line *query-io*))) (return))))
