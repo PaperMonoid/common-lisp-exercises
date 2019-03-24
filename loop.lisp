4558597522108499b0ffdc37fe18917117fe7d15
TritiumMonoid <tritiummonoid@gmail.com>
2019-03-23 22:22:21 -0700

    add dolist

--- loop.lisp
+++ loop.lisp
@@ -1,4 +1,5 @@
-(loop for i in '(ichi ni san) do (print i))
+(loop for i in '(ichi ni san)
+      do (print i))
 
 (loop for x from 1 to 100
       for y = (* x 2)
@@ -8,3 +9,6 @@
       collect (list b a) )
 
 (loop repeat 10 do (format t "ok"))
+
+(dolist (item '(ichi ni san))
+  (print item))
