9f6b55e3cf2c10a4174d1da0c4a1b4f4624dd616
TritiumMonoid <tritiummonoid@gmail.com>
2019-03-31 00:40:25 -0700

    refactor monte carlo pi

--- monte-carlo.lisp
+++ monte-carlo.lisp
@@ -1,9 +1,18 @@
-(defun U (inferior superior)
-  (+ inferior (random (- superior inferior))))
+(defun U ()
+  (random 1.0))
 
-(defun E-PI (&optional (n 10000))
-    (let ((i 0.0))
-      (loop repeat n
-	    do (when (<= (+ (expt (U -1.0 1.0) 2.0) (expt (U -1.0 1.0) 2.0)) 1.0)
-		 (incf i 1.0)))
-      (* (/ i n) 4.0)))
+(defun E (X &optional (n 1))
+  (let ((sum 0.0))
+    (loop repeat n
+	  do (incf sum (funcall X)))
+    (/ sum n)))
+
+(defun I ()
+  (cond
+    ((<= (+ (expt (- (* (U) 2) 1) 2) (expt (- (* (U) 2) 1) 2)) 1) 1)
+    (t 0)))
+
+(defun E-PI (&optional (n 1))
+  (* (E #'I n) 4))
+
+(E-PI 10000)
