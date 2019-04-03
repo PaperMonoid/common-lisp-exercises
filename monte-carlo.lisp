0373bff3dbe3b41bc9954965bbf1ad92bb1afdfe
TritiumMonoid <tritiummonoid@gmail.com>
2019-04-02 22:33:05 -0700

    add more monte carlo examples

--- monte-carlo.lisp
+++ monte-carlo.lisp
@@ -1,18 +1,42 @@
 (defun U ()
   (random 1.0))
 
-(defun E (X &optional (n 1))
+(defun avg (X &optional (n 1))
   (let ((sum 0.0))
     (loop repeat n
 	  do (incf sum (funcall X)))
     (/ sum n)))
 
-(defun I ()
+(defun inside-circle? ()
   (cond
     ((<= (+ (expt (- (* (U) 2) 1) 2) (expt (- (* (U) 2) 1) 2)) 1) 1)
     (t 0)))
 
-(defun E-PI (&optional (n 1))
-  (* (E #'I n) 4))
+(defun e-pi (&optional (n 1))
+  (* (avg #'inside-circle? n) 4))
 
-(E-PI 10000)
+(e-pi 10000) ; approximating PI
+
+(defun inside-x? ()
+  (let ((x (U))
+	(y (U)))
+    (cond
+      ((<= y x) 1)
+      (t 0))))
+
+(defun e-x (&optional (n 1))
+  (* (avg #'inside-x? n)))
+
+(e-x 1000) ; approximating integral of y = x
+
+(defun inside-x^2? ()
+  (let ((x (U))
+	(y (U)))
+    (cond
+      ((<= y (expt x 2)) 1)
+      (t 0))))
+
+(defun e-x^2 (&optional (n 1))
+  (* (avg #'inside-x^2? n)))
+
+(e-x^2 1000) ; approximating integral of y = x^2
